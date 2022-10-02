4e86eefbcf3af34b5b6cd760a65732d31e2810cc
D20 <96135484+Dpaste20@users.noreply.github.com>
2022-10-02 23:09:26 +0530

    Update Sketch.py

--- Sketch.py
+++ Sketch.py
@@ -9,4 +9,4 @@ blur = cv2.GaussianBlur(invert, (21, 21), 0)
 invertedblur = cv2.bitwise_not(blur)
 sketch = cv2.divide(grey_img, invertedblur, scale=256.0)
 
-cv2.imwrite("sketch.png", sketch)
+cv2.imwrite("outputImg.png", sketch)
